66c5b08015b56c09cca41d46a304a6f87c729090
Suspir0n <evandrojunior1615@gmail.com>
2021-05-03 19:34:47 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+import codecs
+
+long_description = ''
+with codecs.open('./README.md', encoding='utf-8') as readme_md:
+    long_description = readme_md.read()
+
+setup(
+    name='kitty-mock',
+    packages=find_packages(include=['kitty-mock']),
+    version='0.1.0.0',
+    description='A library in python for simulated API server',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Evandro Silva',
+    license='MIT',
+    install_requires=[],
+    setup_requires=[],
+    tests_requires=['pytest==6.2.3'],
+    tests_suite=['tests'],
+    author_email='evandrojunior1615@gmail.com',
+    url='https://github.com/Suspir0n/kitty-mock',
+    download_url='',
+    python_requires='>=3.6',
+    maintainer='Evandro Silva',
+    maintainer_email='evandrojunior1615@gmail.com',
+)
