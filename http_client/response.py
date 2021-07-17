62881307b8919f02926b59fde0cdf0a5ee4a9dd6
Suspir0n <evandrojunior1615@gmail.com>
2021-07-17 13:10:26 -0300

    using pylint in actions

--- http_client/response.py
+++ http_client/response.py
@@ -1,15 +1,27 @@
+"""
+It receiving the request response and get body.
+"""
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG, datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG,
+    datefmt='%d-%b-%y %H:%M:%S',
+)
 logger = logging.getLogger(__name__)
 
 
 def get_body(receive_response):
+    """
+    It receiving the request response and get body.
+    :param receive_response: json
+    :return: response dictionary
+    """
     logging.info('\033[1;34mReceiving the request response\033[m')
     try:
         response_dictionary = receive_response.json()
         logging.info('\033[1;34mSuccessfully read\033[m')
         return response_dictionary
     except Exception as error:
-        logging.error(f'\033[1;31mFailed to read body, Error: {error}\033[m')
+        logging.error('\033[1;31mFailed to read body, Error: %(error)s\033[m')
         raise Exception(f'failed to read body\n Error: {error}')
